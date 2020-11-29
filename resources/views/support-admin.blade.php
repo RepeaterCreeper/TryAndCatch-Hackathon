88c67bf65872ee9dfa25274aeb88dab6f30d1056
Darwin Marcelo <akosiyawin@gmail.com>
2020-11-29 12:57:34 +0800

    Final

--- resources/views/support-admin.blade.php
+++ resources/views/support-admin.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     @include('flash.message')
-    <div style="height: 80vh; width: 100vw;" class="bg-dark d-flex flex-column align-items-center justify-content-center">
+    <div style="height: 80vh; width: 100vw;" class="d-flex flex-column align-items-center justify-content-center">
        <div class="w-50">
            @foreach ($messages as $message)
                <a href="{{route('chatroom',$message[0]->user_id)}}" class="alert alert-primary d-block text-decoration-none">
